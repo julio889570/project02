14c7197cddd74174b95f79d4bfef36baac059c77
julio889570 <julio889570@gmail.com>
2024-02-05 09:47:15 -0800

    first

--- App.tsx
+++ App.tsx
@@ -6,7 +6,6 @@ export default function App() {
   return (
     <SafeAreaView>
       <ScrollView>
-      <Text>App</Text>
       <FlatCards />
       </ScrollView>
     </SafeAreaView>
--- components/FlatCards.tsx
+++ components/FlatCards.tsx
@@ -4,9 +4,49 @@ import React from 'react'
 export default function FlatCards() {
   return (
     <View>
-      <Text>FlatCards</Text>
+      <Text style={styles.headingText}>Flat Cards</Text>
+      <View style={styles.container}>
+        <View style={[styles.card, styles.cardOne]}>
+            <Text>Red</Text>
+        </View>
+        <View style={[styles.card, styles.cardTwo]}>
+            <Text>Blue</Text>
+        </View>
+        <View style={[styles.card, styles.cardThree]}>
+            <Text>Pink</Text>
+        </View>
+      </View>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+      headingText:{
+        fontSize: 24,
+        fontWeight: 'bold',
+        paddingHorizontal: 8
+      },
+      container: {
+        flex: 1,
+        flexDirection: 'row'
+      },
+      card: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 100,
+        height: 100,
+        borderRadius: 4, 
+        margin: 8
+      },
+      cardOne: {
+        backgroundColor: 'red'
+      },
+      cardTwo: {
+        backgroundColor: 'blue'
+      },
+      cardThree: {
+        backgroundColor: 'pink'
+      }
+
+})
